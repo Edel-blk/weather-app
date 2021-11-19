2ffd7fcf9757f7f7cdd39ff79d66c66350470e17
Edel-blk <55809462+Edel-blk@users.noreply.github.com>
2021-11-18 19:20:43 -0700

    First CSS Styles

--- src/App.js
+++ src/App.js
@@ -9,26 +9,16 @@ function App() {
   return (
     <div className="App">
       <div>
-        <Card
-          max={Cairns.main.temp_max}
-          min={Cairns.main.temp_min}
-          name={Cairns.name}
-          img={Cairns.weather[0].icon}
-          onClose={() => alert(Cairns.name)}
+        <SearchBar
+          onSearch={(ciudad) => alert(ciudad)}
         />
       </div>
-      <hr />
       <div>
         <Cards
           cities={data}
         />
       </div>
-      <hr />
-      <div>
-        <SearchBar
-          onSearch={(ciudad) => alert(ciudad)}
-        />
-      </div>
+      
     </div>
   );
 }
