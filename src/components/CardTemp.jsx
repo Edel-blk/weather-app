0c9e3b0d04421d7372bb880c319ffca6df8f09f0
Edel-blk <55809462+Edel-blk@users.noreply.github.com>
2021-11-18 23:04:02 -0700

    Comienzo de Estilo: Boton

---
+++ src/components/CardTemp.jsx
@@ -0,0 +1,9 @@
+export default function CardTemp({label, value}){
+    return(
+      <div>
+        <label>{label}</label>
+        <span>{value}°</span>
+      </div>
+    )
+  }
+
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './SearchBar.module.css'
-export default function SearchBar(props) {
+
+export default function SearchBar({onSearch}) {
   // acá va tu código
+  
   return (<div className={styles.searchBar}>
-      <div>
-        <h1 className={styles.title}>
-          Aplicacion del Clima
-        </h1>
-      </div>
       <div className={styles.two}>
       <input className={styles.input} placeholder="Ciudad a Buscar" />
-      <button onClick={()=>props.onSearch('Buscando Ciudad')} className={styles.btn}>Agregar</button>    
+      <button onClick={()=>onSearch('Buscando Ciudad')} className={styles.btn}>Agregar</button>    
       </div>
     </div>)
 };
